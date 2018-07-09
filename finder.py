be285db30f6c867641066efce5391162d29c85b1
肖盛锋 <xiaoshengfeng@cloudist.cc>
2018-07-09 21:56:39 +0800

    [ADD] 添加找出 topN 功能

--- finder.py
+++ finder.py
@@ -5,22 +5,25 @@ import os
 import time
 import json
 import argparse
+import commands
+from heapq import heappush, heappop
 
 
-__version__ = 'v0.0.1'
+__version__ = 'v0.1.0'
 
 MB = 1 << 20
-DEFAULT_LARGER_THAN = 100
 
 HELP_TEXT = """调用 ncdu 来定位文件系统中占用了较大空间的文件、目录，使用前请先安装 ncdu，参考： https://dev.yorhel.nl/ncdu """
 
 
 class FindLargeFiles(object):
     def __init__(self):
+        self.files_to_list = list()
         self.parser = argparse.ArgumentParser(description=HELP_TEXT)
         self.parser.add_argument('path', type=str, nargs='?', default='.', help='要查找的路径')
         self.parser.add_argument('--version', '-V', action='version', version=__version__)
-        self.parser.add_argument('--larger-than', '-M', type=float, default=DEFAULT_LARGER_THAN, help='查找大于多少MB的文件')
+        self.parser.add_argument('--larger-than', '-M', type=float, default=100, help='查找大于多少MB的文件')
+        self.parser.add_argument('--top', '-T', type=int, default=20, help='最后显示多少个文件')
         self.parser.add_argument('--exclude', help='排除的路径')
         self.parser.add_argument('--exclude-caches', action='store_true', help='排除Cache文件夹')
         self.args = self.parser.parse_args()
@@ -37,11 +40,18 @@ class FindLargeFiles(object):
 
         if self.args.exclude_caches:
             ncdu_command += ' --exclude-caches'
-        os.system(ncdu_command)
+        commands.getstatusoutput(ncdu_command)
 
     def remove_ncdu_tmp_file(self):
         if os.path.isfile(self.ncdu_output_file_path):
             os.remove(self.ncdu_output_file_path)
+
+    def select_top_files(self, file_tuple):
+        if len(self.files_to_list) < self.args.top:
+            heappush(self.files_to_list, file_tuple)
+        else:
+            heappush(self.files_to_list, file_tuple)
+            heappop(self.files_to_list)
 
     def recurse_ncdu_dir_obj(self, parent_dir_name, file_or_dir_list_obj):
         for file_or_dir_obj in file_or_dir_list_obj:
@@ -49,7 +59,7 @@ class FindLargeFiles(object):
                 file_name = os.path.join(parent_dir_name, file_or_dir_obj.get('name', ''))
                 file_size = file_or_dir_obj.get('asize', 0)
                 if file_size > self.args.larger_than * MB:
-                    print "%s   %s(MB)" % (file_name, round(file_size / MB, 2))
+                    self.select_top_files((file_size, file_name))
             else:  # list 的情况
                 file_name = os.path.join(parent_dir_name, file_or_dir_obj[0].get('name', ''))
                 self.recurse_ncdu_dir_obj(file_name, file_or_dir_obj)
@@ -61,10 +71,16 @@ class FindLargeFiles(object):
             dir_name = dirs_obj[0].get('name', '')
             self.recurse_ncdu_dir_obj(dir_name, dirs_obj)
 
+    def output(self):
+        self.files_to_list.sort(reverse=True)
+        for file_size, file_name in self.files_to_list:
+            print "%s   %.2f(MB)" % (file_name, (file_size / MB))
+
 
 if __name__ == '__main__':
     finder = FindLargeFiles()
     finder.call_ncdu()
     finder.parse_ncdu_output_json_file()
+    finder.output()
     finder.remove_ncdu_tmp_file()
 
